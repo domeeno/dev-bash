2eaaa3cf691ec474104ca692ffad20f2908bd243
domeeno <dominicflocea.dev@gmail.com>
2024-05-03 20:26:54 +0300

    add header lines to env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Author: Flocea Dominic https://github.com/domeeno
+
+# Source file for some common variables used in install.sh/uninstall.sh
+
 export filename="dev"
 export filepath="$HOME/.local/bin/$filename"
 
