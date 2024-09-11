c4714b9826e3cf57c6d2c43b16e0a72c1182fde4
domeeno <dominicflocea.dev@gmail.com>
2024-09-11 23:39:46 +0300

    add reinstall scenario for install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ install_local()
 {
   if [ -f "$filepath" ]; then
     echo "interesting, '$filename' is already installed at $filepath"
+    read -p "update the script? (y/n) " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      cp ./dev.sh "$filepath"
+    fi
     return 0
   fi
 
