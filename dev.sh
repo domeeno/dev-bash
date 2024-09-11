b4cd49816703a73a60c11017d33c77d01960bde4
domeeno <dominicflocea.dev@gmail.com>
2024-09-11 23:56:34 +0300

    add POC with react template

--- dev.sh
+++ dev.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 # Author: Flocea Dominic https://github.com/domeeno
 
-echo "Hello world!"
+
+prompt_hello() {
+  read -p "Hi Dom! You want to start a new project? (y/n): " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    init_new_project
+  else
+    echo "Ah, okay, until next time!"
+  fi
+}
+
+init_new_project() {
+  read -p "Enter project name: " project_name
+
+  read -p $'What technology?\n- cpp\n- react\n- python\n' -r
+  echo "$project_name"
+  echo "$REPLY"
+  if [[ $REPLY == "react" ]]
+  then
+    git clone git@github.com:domeeno/domeeno-react-template.git "$project_name"
+  else
+    echo "not yet implemented"
+  fi
+}
+
+prompt_hello
+
 
